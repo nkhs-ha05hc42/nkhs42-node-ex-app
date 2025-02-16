9e5bfba669c75336340f40053096619c43cb3336
y-ishii-code <yuya.ishii.engineer@gmail.com>
2025-02-16 17:16:29 +0900

    fix: cors

--- server/app.mjs
+++ server/app.mjs
@@ -1,12 +1,16 @@
 import express from "express"
+import cors from "cors"
 import routers from "./routers.mjs"
 
 const app = express()
 const port = 3000
 
+app.use(cors())
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
